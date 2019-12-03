7f7729eeb0627225e945b512f0e3ba9217c00004
Thiago Almeida <tbalmeida@gmail.com>
2019-12-03 19:39:42 +0000

    changes on  packing-list.js

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,16 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
+/* FOR version
 console.log("Kitchen stuff to pack:");
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
+}
+*/
+
+// WHILE version
+console.log("Kitchen stuff to pack:");
+let i = 0;
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
 }
